8e4d8c154ee4a0893eede50cdd5d2701b8e5fc68
Timur Catakli <timurcatakli@gmail.com>
2017-11-30 20:07:54 -0800

    reorganize navigation

--- src/pages/SubPageNavigation.js
+++ src/pages/SubPageNavigation.js
@@ -47,6 +47,13 @@ class SubPageNavigation extends Component {
     const { menuCallToAction } = config
     const menuItems = this.renderMenuItems(subpages)
     const callToAction = this.renderCallToAction(menuCallToAction)
+    const languageOptions = [
+      { key: 'Arabic', text: 'Arabic', value: 'Arabic' },
+      { key: 'Arabic1', text: 'Arabic1', value: 'Arabic1' },
+      { key: 'Arabic2', text: 'Arabic2', value: 'Arabic2' },
+      { key: 'Arabic3', text: 'Arabic3', value: 'Arabic3' }
+    ]
+
     return (
       <div>
         <Grid>
@@ -92,53 +99,50 @@ class SubPageNavigation extends Component {
 
           <Grid.Row only="mobile">
             <Grid.Column>
-              <Menu
-                size="small"
-                secondary
-              >
-                <Menu.Item header color="orange">
-                  <img src="https://react.semantic-ui.com/logo.png" /> &nbsp;{' '}
-                  {siteLabel}
-                </Menu.Item>
-                <Menu.Item link name="home" as={NavLink} exact to="/" />
-                <Menu.Item link name="home1" as={NavLink} exact to="/" />
-                <Menu.Item link name="home2" as={NavLink} exact to="/" />
-                <Menu.Item link name="home3" as={NavLink} exact to="/" />
-                <Menu.Item link name="home4" as={NavLink} exact to="/" />
-                <Menu.Item link name="home5" as={NavLink} exact to="/" />
-                {menuItems}
-              </Menu>
+              <Dropdown
+                button
+                className='icon'
+                floating
+                labeled
+                icon='world'
+                options={languageOptions}
+                search
+                text='Select Language'
+              />
             </Grid.Column>
           </Grid.Row>
 
           {/* <Grid.Row only="mobile">
             <Grid.Column>
               <Menu secondary fixed="top" style={{ backgroundColor: 'gold' }}>
-                <Menu.Item header color="orange">
-                  {siteLabel}
-                </Menu.Item>
-                <Menu.Item>
-                  <Dropdown item text="Categories">
-                    <Dropdown.Menu>
-                      <Dropdown.Item>Electronics</Dropdown.Item>
-                      <Dropdown.Item>Automotive</Dropdown.Item>
-                      <Dropdown.Item>Home</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </Menu.Item>
+            <Menu.Item header color="orange">
+            {siteLabel}
+            </Menu.Item>
+            <Menu.Item>
+            <Dropdown item text="Categories">
+            <Dropdown.Menu>
+            <Dropdown.Item>Electronics</Dropdown.Item>
+            <Dropdown.Item>Automotive</Dropdown.Item>
+            <Dropdown.Item>Home</Dropdown.Item>
+            </Dropdown.Menu>
+            </Dropdown>
+            </Menu.Item>
               </Menu>
             </Grid.Column>
           </Grid.Row> */}
 
           <Grid.Row>
             <Grid.Column>
-              <div className="subpage-header-container" style={{backgroundColor: 'gold'}}>
+              <div
+                className="subpage-header-container"
+                style={{ backgroundColor: 'gold' }}
+              >
                 <Segment basic>
                   <Container>
-                    <Grid verticalAlign='middle'>
+                    <Grid verticalAlign="middle">
                       <Grid.Row>
-                        <Grid.Column width={16} textAlign="center">
-                          <Header as={'h2'}>alskdjaslkdjasd</Header>
+                        <Grid.Column width={16}>
+                          <Header as={'h2'} style={{color: 'gold'}}>PAGE TITLE</Header>
                         </Grid.Column>
                       </Grid.Row>
                     </Grid>
